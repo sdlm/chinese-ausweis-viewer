9923c30c168a12edcba810317554cec4c5b68c28
sdlm <sdlmerr@yandex.ru>
2019-03-10 17:39:45 +0200

    Add script for test web-app

---
+++ predict_000.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+http -d -o predict.jpg -f POST http://127.0.0.1:8000/predict/ file@./chinese_ausweis_viewer/data/train/original_sample_256.jpg && display ./predict.jpg && rm -rf predict.jpg
