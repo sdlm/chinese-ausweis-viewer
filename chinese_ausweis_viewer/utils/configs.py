e73a7c7253b7532cccd38e03f7427c97409b4abc
arseny.kuznetsov <arseny.kuznetsov@exness.com>
2019-04-03 10:04:37 +0300

    Fix template path

--- chinese_ausweis_viewer/utils/configs.py
+++ chinese_ausweis_viewer/utils/configs.py
@@ -5,7 +5,7 @@ GI_IMAGES_PATH = './data/images'
 ORIGINAL_SMPL_PATH = './data/train/original_sample_blur_1000.jpg'
 ORIGINAL_MASK_PATH = './data/train/original_sample_mask_inv_1000.jpg'
 
-CARD_TEMPLATE_PATH = './data/Chinese_card_constructor2.png'
+CARD_TEMPLATE_PATH = './data/Chinese_card_constructor.png'
 FACE_DIR_PATH = 'data/face300_mod2/'
 TRUE_MASK_PATH = 'data/Chinese_card_constructor_mask.png'
 
